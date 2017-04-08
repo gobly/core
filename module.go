60b4559f84609cf32ed5b6e02deb090e070c3193
The Ranger <ranger@risk.ee>
2017-04-08 17:31:31 +0300

    Use prefix during module registration. Delegate unit and module registration to module itself.

--- module.go
+++ module.go
@@ -1,27 +1,34 @@
 package core
 
+import "fmt"
+
 type Module struct {
 	Name  string
 	Units map[string]*Unit
+
+	prefix string // URL prefix that will be prepended to all URL-s for this module
 }
 
 // Functional unit that will be exposed in main menu
 type Unit struct {
 	Name     string
 	Children map[string]*Unit
+
+	prefix string // URL prefix that will be prepended to all URL-s for this unit
 }
 
-func (app *Application) RegisterModule(name string, url string) *Module {
-	app.Modules[url] = &Module{name, make(map[string]*Unit)}
-	return app.Modules[url]
+func (app *Application) RegisterModule(name string, prefix string, url string) *Module {
+	app.Modules[prefix+url] = &Module{name, make(map[string]*Unit), prefix}
+	return app.Modules[prefix+url]
 }
 
 func (module *Module) CreateUnit(name string, url string) *Unit {
-	module.Units[url] = &Unit{name, make(map[string]*Unit)}
-	return module.Units[url]
+	module.Units[module.prefix+url] = &Unit{name, make(map[string]*Unit), module.prefix + url}
+	fmt.Println(module.Units)
+	return module.Units[module.prefix+url]
 }
 
 func (unit *Unit) AddChild(name string, url string) *Unit {
-	unit.Children[url] = &Unit{name, make(map[string]*Unit)}
-	return unit.Children[url]
+	unit.Children[unit.prefix+url] = &Unit{name, make(map[string]*Unit), unit.prefix + url}
+	return unit.Children[unit.prefix+url]
 }
