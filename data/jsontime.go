b43860ba47ab2822c727886a2401864270a36066
The Ranger <ranger@risk.ee>
2017-06-08 22:54:14 +0300

    Use custom time serialization for JSON

---
+++ data/jsontime.go
@@ -0,0 +1,14 @@
+package json
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Time struct {
+	time.Time
+}
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.Format(time.RFC3339))
+}
